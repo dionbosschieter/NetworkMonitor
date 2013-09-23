0d7861072fdb98490d094a8211c4123a11fe781f
Dion Bosschieter <dionbosschieter@gmail.com>
2013-09-23 21:53:09 +0200

    Comments toegevoegd

--- src/Main.py
+++ src/Main.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
+#
+
+# Auteurs: {dionbosschieter,timodekker}
+#  
+# Dit programma is bedoeld voor school, we gaan hier gebruik maken van scapy en ncurses. 
+# Er komt een Agent en een Client dus een soort van server<->client doormiddel van XML of json. 
+# Mijn voorkeur gaat uit naar json.
+#   
+# Voor de licensering zie het bestand LICENSE, er is gebruik gemaakt van een MIT licentie
+#   
+# TODO:
+# Standaard pakketten analyseren en kijken aan welke pakketten we het meeste verkeer kwijt zijn.
+# Thread maken tijdens het binnenhalen van een pakketje.
 
 version = 0.1
 
